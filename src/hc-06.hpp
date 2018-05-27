fa91353a9f243ebeeb9bb02fbe3a573b3f23b70f
Olivier Verwoerd <olivier.verwoerd@student.hu.nl>
2018-05-28 00:44:21 +0200

    better doxygen

--- src/hc-06.hpp
+++ src/hc-06.hpp
@@ -33,6 +33,7 @@ public:
     /// Used to connect to other devices
     /// \details
     /// Connect to the specified device ID. To get the device ID use function "search".
+    /// @param[in] device ID
     void connect(int deviceID);
 
     /// \brief
@@ -41,46 +42,56 @@ public:
 
     /// \brief
     /// Used to read the name of this device.
+    /// @return current device name
     uint8_t* getName();
 
     /// \brief
     /// Used to read the status of the HC-06 chip.
+    /// @return Int of current status
     int getStatus();
 
     /// \brief
     /// Used to pair with other devices.
     /// \details
     /// Pair with the specified device ID. To get the device ID use function "search".
+    /// @param[in] device ID
+
     void pair(int deviceID);
     
     /// \brief
     /// Used to retrieve the message.
     /// \details
     /// Provide this function with own data buffer. The buffer will hold the received message.
+    /// @param[in] Data
     void receive(uint8_t *data);
 
     /// \brief
     /// Used to discover any discoverable devices.
     /// \details
     /// This function will return a device id for all discovered devices. Use this ID to connect.
+    /// @return discovered device IDs
     uint8_t* search();
 
     /// \brief
     /// Used to send data to other devices.
     /// \details
     /// This function will send the data provided.
+    /// @param[in] Data
     void send(uint8_t *data);
 
     /// \brief
     /// Used to set the baud rate of the connection.
+    /// @param[in] baud rate
     void setBaud(int baud);
 
     /// \brief
-    /// Used to set the name of the chip.
+    /// Used to set the name of the chip. This is not an ID
+    /// @param[in] name of the bluetooth module
     void setName(const uint8_t *name);
 
     /// \brief
     /// choose whether the chip will be discoverable or not.
+    /// @param[in] bool true or false
     void setVisibility(bool visible);
 
 };
