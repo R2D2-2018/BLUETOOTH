633503e31211a070a9bc12a9debbc5e66e1dc57b
Laurens van der Sluis <vandersluis.l@outlook.com>
2018-05-31 14:31:14 +0200

    Corrected code style.

--- src/hc-06.cpp
+++ src/hc-06.cpp
@@ -12,54 +12,54 @@
  * @file hc-06.cpp
  */
 
-
 #include "hc-06.hpp"
 
-HC06::HC06(){}
+HC06::HC06() {
+}
 
-void HC06::connect(int deviceID){
+void connect(int deviceID) {
     hwlib::cout << "Connect to device: " << deviceID << '\n';
 }
 
-void HC06::disconnect(){
+void disconnect() {
     hwlib::cout << "Disconnect\n";
 }
 
-uint8_t* HC06::getName(){
+uint8_t *getName() {
     hwlib::cout << "Get name\n";
     return name;
 }
 
-int HC06::getStatus(){
+int getStatus() {
     hwlib::cout << "Get status\n";
     return 0;
 }
 
-void HC06::pair(int deviceID){
+void pair(int deviceID) {
     hwlib::cout << "Pair with device: " << deviceID << '\n';
 }
 
-void HC06::receive(uint8_t *data){
+void receive(uint8_t *data) {
     hwlib::cout << "Receive data\n";
 }
 
-uint8_t* HC06::search(){
+uint8_t *search() {
     hwlib::cout << "Search for devices\n";
     return discoveredDevices;
 }
 
-void HC06::send(uint8_t *data){
+void send(uint8_t *data) {
     hwlib::cout << "Sending data\n";
 }
 
-void HC06::setBaud(int baud){
+void setBaud(int baud) {
     hwlib::cout << "Set baud rate to: " << baud << '\n';
 }
 
-void HC06::setName(const uint8_t *name){
+void setName(const uint8_t *name) {
     hwlib::cout << "Set name\n";
 }
 
-void HC06::setVisibility(bool visible){
+void setVisibility(bool visible) {
     hwlib::cout << "Set visibility\n";
 }
